8fde87713d42eea4d5d011ab15d8728d4d11e149
Lim <lim12001@byui.edu>
2018-06-29 18:09:43 -0600

    added RESTful API for workouts

--- Workout/server.js
+++ Workout/server.js
@@ -9,6 +9,7 @@ const pool = new Pool({connectionString: connectionString});
 app.set("port", (process.env.PORT || 5000));
 
 app.get("/getUsers", getUsers); // getUsers/:id
+app.get("/getWorkouts", getWorkouts); 
 
 app.listen(app.get("port"), function() {
 	console.log("Now listening for connection on port: ", app.get("port")); 
@@ -21,6 +22,30 @@ function getUsers(req, res) {
 	console.log("logging ", id);
 	
 	getUserFromDb(id, function(error, result) { 
+	
+		if (error || result == null || result.length != 1)
+		{
+			res.status(500).json({success:false, data: error});
+		}
+		else {
+			
+			console.log("back from getUserFromDb function with results: ", result);
+			res.json(result[0]);
+		}
+		
+		
+		
+	});
+	
+}
+
+function getWorkouts(req, res) {
+	console.log("Getting workout info");
+	
+	var id = req.query.id; //req.params.id; //
+	console.log("logging ", id);
+	
+	getWorkoutFromDb(id, function(error, result) { 
 	
 		if (error || result == null || result.length != 1)
 		{
@@ -55,4 +80,22 @@ function getUserFromDb(id, callback) {
 		callback(null, result.rows);
 	});
 	
+}
+
+function getWorkoutFromDb(id, callback) {
+	console.log("getWorkoutFromDb function called with id: ", id);
+	
+	var sql = "SELECT * FROM workout WHERE id = $1::int";
+	var params = [id];
+	
+	pool.query(sql, params, function(err, result) {
+		if (err) {
+			console.log("An error with DB occured: ", err);
+			callback(err, null);
+		}
+		
+		console.log("found DB result: " + JSON.stringify(result.rows));
+		
+		callback(null, result.rows);
+	});
 }
