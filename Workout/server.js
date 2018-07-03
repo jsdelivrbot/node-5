38bffbf2debd52e539467ef3653490cf2f029423
Lim <lim12001@byui.edu>
2018-07-02 18:24:29 -0600

    adding Workout Project public files

--- Workout/server.js
+++ Workout/server.js
@@ -8,6 +8,10 @@ const pool = new Pool({connectionString: connectionString});
 
 app.set("port", (process.env.PORT || 5000));
 
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 app.get("/getUsers", getUsers); // getUsers/:id
 app.get("/getWorkouts", getWorkouts); 
 
